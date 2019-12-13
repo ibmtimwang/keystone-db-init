ee2163699e58da456606f3c873c5ae5d21aae8d3
Adam Young <ayoung@redhat.com>
2019-12-13 14:04:13 -0500

    removed all but db-sync call

--- configure_keystone.sh
+++ configure_keystone.sh
@@ -1,32 +1,13 @@
 #!/bin/bash
 
-#MYSQL_HOST=keystone-mariadb
-MYSQL_PORT=3306
-MYSQL_PASSWORD=keystone 
-MYSQL_HOST=10.131.0.139
+set -x
 
-if [ -z "$MYSQL_PASSWORD" ]
-then MYSQL_PASSWORD=$( cat /etc/keystone/dbpass.txt )
-fi
+echo -n Database
 
-cat /var/`whoami`
-
-
-echo -n Database 
-mysql -h $MYSQL_HOST  -P$MYSQL_PORT --user=keystone --password=$MYSQL_PASSWORD < keystone-configure.sql
+echo -n db-sync
+keystone-manage db_sync  2>&1
 echo " [COMPLETE]"
- 
-echo -n "configuration "
-openstack-config  --set  /etc/keystone/keystone.conf database connection mysql+pymysql://keystone:$MYSQL_PASSWORD@$MYSQL_HOST/keystone
-DATABASE_CONN=`openstack-config  --get  /etc/keystone/keystone.conf database connection `
-echo $DATABASE_CONN
- 
-echo " [COMPLETE]"
- 
-echo -n "db-sync "
-keystone-manage db_sync
-echo " [COMPLETE]"
- 
+
 echo -n "bootstrap "
 keystone-manage bootstrap --bootstrap-password=FreeIPA4All
 echo " [COMPLETE]"
